8afacdcaaad72b5664c448a2bbb75d21d4cda31a
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2019-10-14 20:07:42 -0300

    Fix vertical view adaptation

--- App.js
+++ App.js
@@ -33,40 +33,28 @@ export class App extends Component {
 
   render() {
     return (
-      <>
-        <SafeAreaView>
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic">
-            {global.HermesInternal == null ? null : (
-              <View>
-                <Text style={styles.footer}>Engine: Hermes</Text>
-              </View>
-            )}
-            <View style={styles.container}>
-              <Display value={this.state.displayValue}/>
-              <View style={styles.buttons}>
-                <Button label='AC' triple onClick={ this.clearMemory }></Button>
-                <Button label='/' operation onClick={ this.setOperation }></Button>
-                <Button label='7' onClick={ this.addDigit }></Button>
-                <Button label='8' onClick={ this.addDigit }></Button>
-                <Button label='9' onClick={ this.addDigit }></Button>
-                <Button label='*' operation onClick={ this.setOperation }></Button>
-                <Button label='4' onClick={ this.addDigit }></Button>
-                <Button label='5' onClick={ this.addDigit }></Button>
-                <Button label='6' onClick={ this.addDigit }></Button>
-                <Button label='-' operation onClick={ this.setOperation }></Button>
-                <Button label='1' onClick={ this.addDigit }></Button>
-                <Button label='2' onClick={ this.addDigit }></Button>
-                <Button label='3' onClick={ this.addDigit }></Button>
-                <Button label='+' operation onClick={ this.setOperation }></Button>
-                <Button label='0' double onClick={ this.addDigit }></Button>
-                <Button label='.' onClick={ this.addDigit }></Button>
-                <Button label='=' operation onClick={ this.setOperation }></Button>
-              </View>
-            </View>
-          </ScrollView>
-        </SafeAreaView>
-      </>
+      <View style={styles.container}>
+        <Display value={this.state.displayValue}/>
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={ this.clearMemory }></Button>
+          <Button label='/' operation onClick={ this.setOperation }></Button>
+          <Button label='7' onClick={ this.addDigit }></Button>
+          <Button label='8' onClick={ this.addDigit }></Button>
+          <Button label='9' onClick={ this.addDigit }></Button>
+          <Button label='*' operation onClick={ this.setOperation }></Button>
+          <Button label='4' onClick={ this.addDigit }></Button>
+          <Button label='5' onClick={ this.addDigit }></Button>
+          <Button label='6' onClick={ this.addDigit }></Button>
+          <Button label='-' operation onClick={ this.setOperation }></Button>
+          <Button label='1' onClick={ this.addDigit }></Button>
+          <Button label='2' onClick={ this.addDigit }></Button>
+          <Button label='3' onClick={ this.addDigit }></Button>
+          <Button label='+' operation onClick={ this.setOperation }></Button>
+          <Button label='0' double onClick={ this.addDigit }></Button>
+          <Button label='.' onClick={ this.addDigit }></Button>
+          <Button label='=' operation onClick={ this.setOperation }></Button>
+        </View>
+      </View>
     );
   }
 };
